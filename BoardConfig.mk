efdceda21dacb9a680a6226381e9c2427d09f037
Mehmet <you@example.com>
2014-11-06 12:34:04 +0200

    edited

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -158,7 +158,7 @@ BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
 BOARD_VOLD_MAX_PARTITIONS := 30
 TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun%d/file
 
- SELinux
+# SELinux
 BOARD_SEPOLICY_DIRS += \
 	device/samsung/matissewifi/sepolicy
 
